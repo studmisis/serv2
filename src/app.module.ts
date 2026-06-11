172394fc135e45d72b784aa25c5c14af0161f4ad
studmisis <studmisis@bk.ru>
2026-06-11 19:47:55 +0300

    auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
 import { PrismaModule } from './prisma/prisma.module';
-
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [UsersModule, TasksModule, PrismaModule],
+  imports: [UsersModule, TasksModule, PrismaModule, AuthModule],
   controllers: [],
 })
 export class AppModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -8,5 +8,6 @@ import { UsersRepository } from './users.repository';
   imports: [PrismaModule],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
+  exports: [UsersService],
 })
 export class UsersModule {}
